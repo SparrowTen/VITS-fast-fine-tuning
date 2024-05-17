a0c411e5b2c63f16e2bdf91bb67d659dbd083847
poyu39 <bruce920309@gmail.com>
2024-05-17 22:09:25 +0800

    feat: 新增 split_anno.py
    將 whisper 生成文字整理成 cli_refake.py 的讀取格式

---
+++ scripts/split_anno.py
@@ -0,0 +1,24 @@
+# ./custom_character_voice/VO_paimon/processed_690.wav|VO_paimon|[ZH]应该是一个乐器,只不过太久没人保养,好像已经没法弹奏了。[ZH]
+import os
+import argparse
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='split anno')
+    parser.add_argument('--txt', type=str, help='anno file')
+    parser.add_argument('--speaker', type=str, help='speaker name')
+    args = parser.parse_args()
+    lang = ['[ZH]', '[EN]', '[JA]']
+    
+    with open(args.txt, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+        for line in lines:
+            raw = line.strip().split('|')
+            orgin = raw[0].split('/')[-1]
+            sub = raw[2]
+            for l in lang:
+                if l in sub:
+                    sub = sub.replace(l, '')
+                    break
+            with open(f'./sub/{args.speaker}_sub.txt', 'a', encoding='utf-8') as f:
+                f.write(f'{orgin}|{sub}\n')
